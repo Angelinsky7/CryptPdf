029b6ee4156779fae8c099ad395f0a980120d176
demarco <steven.demarco@while-not-false.ch>
2023-02-21 22:47:54 +0100

    Add project files.

---
+++ CryptPdf/Options.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine.Text;
+using CommandLine;
+
+namespace CryptPdf {
+    public class Options {
+
+        [Option('i', "input", Required = true, HelpText = "Input PDF File.")]
+        public String Input { get; set; } = default!;
+
+        [Option('o', "output", Required = true, HelpText = "Output PDF File.")]
+        public String Output { get; set; } = default!;
+
+        [Option('p', "password", Required = false, HelpText = "New Password for the Output PDF File.")]
+        public String Password { get; set; } = default!;
+
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public Boolean Verbose { get; set; }
+
+    }
+}
---
+++ CryptPdf/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using CommandLine;
+using CryptPdf;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+using PdfSharpCore.Pdf.Security;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(o => {
+
+        String tmpFile = $"{Path.GetTempPath()}{Path.GetRandomFileName()}";
+        File.Copy(o.Input, tmpFile, true);
+
+        PdfDocument pdfDocument = PdfReader.Open(tmpFile, PdfDocumentOpenMode.Modify);
+        PdfSecuritySettings securitySettings = pdfDocument.SecuritySettings;
+
+        String? password = o.Password;
+        if (String.IsNullOrEmpty(password)) {
+            while (String.IsNullOrEmpty(password)) {
+                Console.Write("Password: ");
+                password = readKeys();
+                Console.Write("Password confirmation: ");
+                String? passwordConfirmation = readKeys();
+                if (passwordConfirmation != password) {
+                    Console.WriteLine("Password don't match !");
+                    password = null;
+                }
+            }
+        }
+
+        securitySettings.UserPassword = password;
+
+        securitySettings.PermitAccessibilityExtractContent = false;
+        securitySettings.PermitAnnotations = false;
+        securitySettings.PermitAssembleDocument = false;
+        securitySettings.PermitExtractContent = false;
+        securitySettings.PermitFormsFill = false;
+        securitySettings.PermitFullQualityPrint = false;
+        securitySettings.PermitModifyDocument = false;
+        securitySettings.PermitPrint = false;
+        securitySettings.PermitFullQualityPrint = false;
+
+        pdfDocument.Save(o.Output);
+
+        File.Delete(tmpFile);
+    });
+
+static String? readKeys() {
+    StringBuilder sb = new StringBuilder();
+
+    ConsoleKeyInfo keyInfo;
+    do {
+        keyInfo = Console.ReadKey(intercept: true);
+
+        if (keyInfo.Key == ConsoleKey.Backspace && sb.Length > 0) {
+            Console.Write("\b \b");
+            sb.Remove(sb.Length - 1, 1);
+        } else if (!char.IsControl(keyInfo.KeyChar)) {
+            Console.Write("*");
+            sb.Append(keyInfo.KeyChar);
+        }
+    } while (keyInfo.Key != ConsoleKey.Enter);
+
+    Console.WriteLine();
+
+    return sb.ToString();
+}
